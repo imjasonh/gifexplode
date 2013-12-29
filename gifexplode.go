130da870421b20507e2619e87d17ef4417c92b2f
Jason Hall <imjasonh@gmail.com>
2013-12-29 18:32:13 -0500

    Display frames as png data URIs

--- gifexplode.go
+++ gifexplode.go
@@ -2,10 +2,27 @@ package gifexplode
 
 import (
 	"appengine"
-	
+
+	"bytes"
+	"encoding/base64"
+	"fmt"
+	"html/template"
 	"image/gif"
+	"image/png"
 	"net/http"
 )
+
+const (
+	bufSize = 2 << 17
+)
+
+var tmpl = template.Must(template.New("tmpl").Parse(`
+<html><body>
+{{range .Frames}}
+<img src="{{.}}" /><br />
+{{end}}
+</body></html>
+`))
 
 func init() {
 	http.HandleFunc("/upload", upload)
@@ -16,17 +33,31 @@ func upload(w http.ResponseWriter, r *http.Request) {
 	f, _, err := r.FormFile("file")
 	if err != nil {
 		c.Errorf("formfile: %v", err)
-		http.Error(w, "", http.StatusBadRequest)
+		http.Error(w, "No file specified", http.StatusBadRequest)
+		return
 	}
 	defer f.Close()
 	g, err := gif.DecodeAll(f)
 	if err != nil {
 		c.Errorf("gif decode: %v", err)
-		http.Error(w, "", http.StatusBadRequest)
+		http.Error(w, "Error decoding GIF", http.StatusBadRequest)
+		return
 	}
 	fs := []string{}
 	for _, i := range g.Image {
-		f := "data:image/gif;base64,"
-		fs = append(fs, f)
+		buf := bytes.NewBuffer(make([]byte, 0, bufSize))
+		// TODO: Upgrade to go1.2 and gif.Encode
+		err = png.Encode(buf, i)
+		if err != nil {
+			c.Errorf("png encode: %v", err)
+			http.Error(w, "Error encoding", http.StatusInternalServerError)
+			return
+		}
+		dataURI := fmt.Sprintf("data:image/png;base64,%s", base64.StdEncoding.EncodeToString(buf.Bytes()))
+		fmt.Println(dataURI)
+		fs = append(fs, dataURI)
 	}
+	tmpl.Execute(w, struct {
+		Frames []string
+	}{fs})
 }
