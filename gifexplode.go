6ef8db2fc752ab518d117616dae766f2724c7ca1
Jason Hall <imjasonh@gmail.com>
2013-12-30 20:26:32 -0500

    Readd support for fetch in delayed version, show Loading while the gif is being exploded

--- gifexplode.go
+++ gifexplode.go
@@ -6,6 +6,7 @@ import (
 	"appengine/channel"
 	"appengine/datastore"
 	"appengine/delay"
+	"appengine/urlfetch"
 
 	"bytes"
 	"encoding/base64"
@@ -17,59 +18,53 @@ import (
 	"image/png"
 	"io"
 	"net/http"
+	"strings"
 	"text/template"
 )
 
-const (
-	// Maximum single frame size
-	maxFrameSize = 1 << 18 // 256 KB
-)
+// Maximum single frame size
+const maxFrameSize = 1 << 18 // 256 KB
 
-var later = delay.Func("explode", func(c appengine.Context, cid string, bk appengine.BlobKey) error {
-	if _, err := blobstore.Stat(c, bk); err == datastore.ErrNoSuchEntity {
-		return nil
-	}
-	frames, err := frames(c, blobstore.NewReader(c, bk))
-	if err != nil {
-		return err
-	}
-	l := len(frames)
-	type data struct {
-		I, L int
-		F    string
-	}
-	for i := 0; i < l; i++ {
-		if err := channel.SendJSON(c, cid, data{i, l, frames[i]}); err != nil {
-			return err
-		}
-	}
-	return blobstore.Delete(c, bk)
-})
+type data struct {
+	I, L int
+	F    string
+}
 
 func init() {
 	http.HandleFunc("/", in)
 	http.HandleFunc("/upload", upload)
+	http.HandleFunc("/fetch", fetch)
 }
 
 var inTmpl = template.Must(template.New("in").Parse(`
 <html><body>
 <form action="{{.}}" method="POST" id="form" enctype="multipart/form-data">
-<input type="file" name="file" id="file" accept="image/gif"></input>
-<script type="text/javascript">
-document.getElementById('file').onchange = function() {
-  document.getElementById('form').submit();
-};
-</script></form></body></html>
+  <label for="file">Select an animated GIF</label>
+  <input type="file" name="file" id="file" accept="image/gif"></input>
+  <script type="text/javascript">
+  document.getElementById('file').onchange = function() {
+    document.getElementById('form').submit();
+  };
+  </script>
+</form>
+
+<form action="/fetch" method="GET">
+  <label for="url">Specify a URL</label>
+  <input type="text" name="url"></input>
+  <input type="submit"></input>
+</form></body></html>
 `))
 
 var outTmpl = template.Must(template.New("out").Parse(`
 <html><body>
+<div id="loading">Loading...</div>
 <script type="text/javascript" src="/_ah/channel/jsapi"></script>
 <script type="text/javascript">
 document.innerHTML = 'loading...';
 var tok = '{{.Tok}}';
 var s = new goog.appengine.Channel(tok).open();
 s.onmessage = function(m) {
+  document.getElementById('loading').style.display = 'none';
   var d = JSON.parse(m.data);
   for (var i = document.getElementsByTagName('img').length; i < d.L; i++) {
     document.body.appendChild(document.createElement('img'));
@@ -112,11 +107,70 @@ func upload(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Error creating channel", http.StatusInternalServerError)
 		return
 	}
-	later.Call(c, cid, f[0].BlobKey)
+	blobLater.Call(c, cid, f[0].BlobKey)
 	outTmpl.Execute(w, struct {
 		Tok string
 	}{tok})
 }
+
+var blobLater = delay.Func("bloblater", func(c appengine.Context, cid string, bk appengine.BlobKey) error {
+	if _, err := blobstore.Stat(c, bk); err == datastore.ErrNoSuchEntity {
+		return nil
+	}
+	fs, err := frames(c, blobstore.NewReader(c, bk))
+	if err != nil {
+		return err
+	}
+	l := len(fs)
+	for i := 0; i < l; i++ {
+		if err := channel.SendJSON(c, cid, data{i, l, fs[i]}); err != nil {
+			return err
+		}
+	}
+	return blobstore.Delete(c, bk)
+})
+
+func fetch(w http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
+	url := r.FormValue("url")
+	if url == "" {
+		http.Error(w, "No URL specified", http.StatusBadRequest)
+		return
+	}
+	if !strings.HasPrefix(url, "http") {
+		url = "http://"+url
+	}
+	cid := appengine.RequestID(c)
+	tok, err := channel.Create(c, cid)
+	if err != nil {
+		c.Errorf("create channel: %v", err)
+		http.Error(w, "Error creating channel", http.StatusInternalServerError)
+		return
+	}
+	fetchLater.Call(c, cid, url)
+	outTmpl.Execute(w, struct {
+		Tok string
+	}{tok})
+}
+
+var fetchLater = delay.Func("fetchlater", func(c appengine.Context, cid, url string) error {
+	resp, err := urlfetch.Client(c).Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	fs, err := frames(c, resp.Body)
+	if err != nil {
+		return err
+	}
+	l := len(fs)
+	for i := 0; i < l; i++ {
+		if err := channel.SendJSON(c, cid, data{i, l, fs[i]}); err != nil {
+			return err
+		}
+	}
+	return nil
+})
 
 func frames(c appengine.Context, r io.Reader) ([]string, error) {
 	g, err := gif.DecodeAll(r)
